d7871784c7e064b17ff3837b4599802755d47ada
BlueFury <bluefurythebest@gmail.com>
2021-01-13 18:01:09 +0530

    optimized code

--- src/app.ts
+++ src/app.ts
@@ -35,11 +35,13 @@ const ul = document.querySelector("ul")!;
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
+  let values: [string, string, number ]
+  values = [tofrom.value, details.value, amount.valueAsNumber]
   let doc: HasFormatter;
   if (type.value === "invoice") {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
   list.render(doc,type.value,'end')
 });
